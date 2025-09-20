406c54f4edb3a83344a79e81ec242ae3af38a4a8
santiago <sanseb290514@gmail.com>
2025-09-20 13:03:51 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,10 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'prueba-tecnica-mpos';
+
 }
